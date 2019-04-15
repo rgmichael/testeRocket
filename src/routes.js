b0fea6c3097ef6abe1a9fa8fbec36281158eff41
rgmichael <rgmichael@gmail.com>
2019-04-14 22:40:48 -0300

    primeiro be

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const multer = require("multer");
+const multerConfig = require("./config/multer");
+
+const routes = express.Router();
+
+const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
+routes.post("/boxes", BoxController.store );
+
+routes.post("/boxes/:id/files", multer(multerConfig).single('file'), FileController.store );
+
+routes.get("/box/:id", BoxController.show );
+/*
+routes.get("/teste", (req, res) => {
+    return res.send("aaaa 2444");
+});
+*/
+module.exports = routes;
